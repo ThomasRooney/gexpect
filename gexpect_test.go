4edbbc42ae5b659772cd6e294a0f3c211aafebcf
Thomas Rooney <thomas.rooney11@imperial.ac.uk>
2015-04-27 18:15:10 +0100

    Made the test framework more in line with standard practices

--- gexpect_test.go
+++ gexpect_test.go
@@ -1,64 +1,63 @@
 package gexpect
 
 import (
-	"log"
 	"strings"
 	"testing"
 )
 
-func TestHelloWorld(*testing.T) {
-	log.Printf("Testing Hello World... ")
+func TestHelloWorld(t *testing.T) {
+	t.Logf("Testing Hello World... ")
 	child, err := Spawn("echo \"Hello World\"")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	err = child.Expect("Hello World")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
-	log.Printf("Success\n")
+	t.Logf("Success\n")
 }
 
-func TestDoubleHelloWorld(*testing.T) {
-	log.Printf("Testing Double Hello World... ")
+func TestDoubleHelloWorld(t *testing.T) {
+	t.Logf("Testing Double Hello World... ")
 	child, err := Spawn(`sh -c "echo Hello World ; echo Hello ; echo Hi"`)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	err = child.Expect("Hello World")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	err = child.Expect("Hello")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	err = child.Expect("Hi")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
-	log.Printf("Success\n")
+	t.Logf("Success\n")
 }
 
-func TestHelloWorldFailureCase(*testing.T) {
-	log.Printf("Testing Hello World Failure case... ")
+func TestHelloWorldFailureCase(t *testing.T) {
+	t.Logf("Testing Hello World Failure case... ")
 	child, err := Spawn("echo \"Hello World\"")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	err = child.Expect("YOU WILL NEVER FIND ME")
 	if err != nil {
-		log.Printf("Success\n")
+		t.Logf("Success\n")
 		return
 	}
-	panic("Expected an error for TestHelloWorldFailureCase")
+	t.Fatal("Expected an error for TestHelloWorldFailureCase")
 }
 
-func TestBiChannel(*testing.T) {
-	log.Printf("Testing BiChannel screen... ")
+func TestBiChannel(t *testing.T) {
+	t.Logf("Testing BiChannel screen... ")
 	child, err := Spawn("screen")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	sender, reciever := child.AsyncInteractChannels()
 	wait := func(str string) {
@@ -78,21 +77,21 @@ func TestBiChannel(*testing.T) {
 	sender <- "times\n"
 	wait("s")
 	sender <- "^D\n"
-	log.Printf("Success\n")
+	t.Logf("Success\n")
 
 }
 
-func TestCommandStart(*testing.T) {
-	log.Printf("Testing Command... ")
+func TestCommandStart(t *testing.T) {
+	t.Logf("Testing Command... ")
 
 	// Doing this allows you to modify the cmd struct prior to execution, for example to add environment variables
 	child, err := Command("echo 'Hello World'")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	child.Start()
 	child.Expect("Hello World")
-	log.Printf("Success\n")
+	t.Logf("Success\n")
 }
 
 var regexMatchTests = []struct {
@@ -108,7 +107,7 @@ var regexMatchTests = []struct {
 }
 
 func TestRegex(t *testing.T) {
-	log.Printf("Testing Regular Expression Matching... ")
+	t.Logf("Testing Regular Expression Matching... ")
 	for _, tt := range regexMatchTests {
 		runTest := func(input string) bool {
 			var match bool
@@ -129,5 +128,5 @@ func TestRegex(t *testing.T) {
 			t.Errorf("Regex Matching [%#q] with pattern [%#q]", tt.bad, tt.re)
 		}
 	}
-	log.Printf("Success\n")
+	t.Logf("Success\n")
 }
