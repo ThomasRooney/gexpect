8cb2c3a640a279b5326deb986db44ad85c57054e
Stefan Junker <mail@stefanjunker.de>
2015-07-31 18:57:32 +0200

    tests: minor fixes

--- gexpect_test.go
+++ gexpect_test.go
@@ -72,10 +72,10 @@ func TestBiChannel(t *testing.T) {
 		}
 	}
 
-	endlChar := fmt.Sprintln("")[0]
-	sender <- fmt.Sprintf("echo%c", endlChar)
+	endlChar := fmt.Sprintln("")
+	sender <- fmt.Sprintf("echo%v", endlChar)
 	wait("echo")
-	sender <- fmt.Sprintf("echo2%c", endlChar)
+	sender <- fmt.Sprintf("echo2%v", endlChar)
 	wait("echo2")
 	child.Close()
 	child.Wait()
@@ -208,7 +208,7 @@ func TestRegexWithOutput(t *testing.T) {
 }
 
 func TestRegexTimeoutWithOutput(t *testing.T) {
-	t.Logf("Testing Regular Expression search with output...")
+	t.Logf("Testing Regular Expression search with timeout and output...")
 
 	seconds := 2
 	timeout := time.Duration(seconds-1) * time.Second
